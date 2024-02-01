660b026a176e6da1db61c65532b97a468cbb47a0
Sebastian Słoma <sebastiansloma@gmail.com>
2024-02-01 21:34:16 +0100

    added theme btn

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,11 @@
 import Image from "next/image";
+import { ThemeToggle } from './components/Themetoggle';
 
 export default function Home() {
   return (
     
     <main>
-      <h1>start project</h1>
+     <ThemeToggle/>
     </main>
   );
 }
