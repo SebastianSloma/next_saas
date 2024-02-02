db671ba77f1cfe48e59f05e6ae4da9ca6b7d7113
Sebastian Słoma <sebastiansloma@gmail.com>
2024-02-02 15:52:43 +0100

    added btns to nav

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -1,13 +1,23 @@
 import Link from 'next/link'
 import React from 'react'
+import { ThemeToggle } from './Themetoggle'
+import { Button } from '@/components/ui/button'
 
 export const NavBar = () => {
   return (
     <nav className='border-b bg-background h-[10vh] flex items-center'>
         <div className='container flex items-center justify-between'>
             <Link href='/'>
-                <h1>Test Nav</h1>
+                <h1 className='font-bold test-3xl'>Saas App</h1>
             </Link>
+
+            <div className='flex items-center gap-x-5'>
+            <ThemeToggle/>
+            <div className='flex items-center gap-x-5'>
+                <Button>Sign In</Button>
+                <Button variant='secondary'>Sign Up</Button>
+            </div>
+            </div>
 
         </div>
     </nav>
