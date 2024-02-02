dc93b10f4f30b1cf651ac5a804abdff8b0edd9b0
Sebastian Słoma <sebastiansloma@gmail.com>
2024-02-02 15:39:53 +0100

    added navbar

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -1,9 +1,15 @@
+import Link from 'next/link'
 import React from 'react'
 
 export const NavBar = () => {
   return (
     <nav className='border-b bg-background h-[10vh] flex items-center'>
+        <div className='container flex items-center justify-between'>
+            <Link href='/'>
+                <h1>Test Nav</h1>
+            </Link>
 
+        </div>
     </nav>
   )
 }
