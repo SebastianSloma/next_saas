0fe42132a641761975b803440b58d8c09f6f1699
Sebastian Słoma <sebastiansloma@gmail.com>
2024-02-10 19:14:22 +0100

    add map

--- app/components/DashboardNav.tsx
+++ app/components/DashboardNav.tsx
@@ -2,6 +2,8 @@
 
 import React from 'react';
 import { CreditCard, Home, Settings } from 'lucide-react';
+import Link from 'next/link';
+import { cn } from '@/lib/utils';
 
 export const navItems = [
 	{ name: 'Home', href: '/dashboard', icon: Home },
@@ -10,12 +12,18 @@ export const navItems = [
 ];
 
 const DashboardNav = () => {
-	return <nav className='grid items-start gap-2'>
-    {navItems.map((item, index)=>(
-      <Link key
-    ))}
-
-  </nav>;
+	return (
+		<nav className='grid items-start gap-2'>
+			{navItems.map((item, index) => (
+				<Link key={index} href={item.href}>
+					<span className={cn()}>
+						<item.icon className='mr-2 h-4 w-4' />
+						<span>{item.name}</span>
+					</span>
+				</Link>
+			))}
+		</nav>
+	);
 };
 
 export default DashboardNav;
