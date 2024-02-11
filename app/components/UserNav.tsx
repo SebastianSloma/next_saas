bc0c78e02959f81ed76e9fdfc704e2c4a397b90b
Sebastian Słoma <sebastiansloma@gmail.com>
2024-02-11 12:14:04 +0100

    add comp.

--- app/components/UserNav.tsx
+++ app/components/UserNav.tsx
@@ -1,3 +1,18 @@
-export function UserNav(){
-    
+import { Avatar, AvatarImage } from '@/components/ui/avatar';
+import { Button } from '@/components/ui/button';
+import {
+	DropdownMenu,
+	DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu';
+
+export function UserNav() {
+	<DropdownMenu>
+		<DropdownMenuTrigger asChild>
+			<Button>
+				<Avatar>
+					<AvatarImage />
+				</Avatar>
+			</Button>
+		</DropdownMenuTrigger>
+	</DropdownMenu>;
 }
