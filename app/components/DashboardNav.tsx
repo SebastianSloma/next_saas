29139b3c6c758a60d921e89d061306e2d06f7b6a
Sebastian Słoma <sebastiansloma@gmail.com>
2024-02-11 11:52:49 +0100

    styled icons

--- app/components/DashboardNav.tsx
+++ app/components/DashboardNav.tsx
@@ -4,6 +4,7 @@ import React from 'react';
 import { CreditCard, Home, Settings } from 'lucide-react';
 import Link from 'next/link';
 import { cn } from '@/lib/utils';
+import { usePathname } from 'next/navigation';
 
 export const navItems = [
 	{ name: 'Home', href: '/dashboard', icon: Home },
@@ -12,11 +13,15 @@ export const navItems = [
 ];
 
 const DashboardNav = () => {
+	const pathname = usePathname()
+
 	return (
 		<nav className='grid items-start gap-2'>
 			{navItems.map((item, index) => (
 				<Link key={index} href={item.href}>
-					<span className={cn()}>
+					<span className={cn(
+						"group flex items-center rounded-md px-3 py-2 text-sm font-medium hover:bg-accent hover:text-accent-foreground"
+					)}>
 						<item.icon className='mr-2 h-4 w-4' />
 						<span>{item.name}</span>
 					</span>
