e7babd16587cee234497966de6275966b49f4957
Sebastian Słoma <sebastiansloma@gmail.com>
2024-02-11 12:03:05 +0100

    s icons

--- app/components/DashboardNav.tsx
+++ app/components/DashboardNav.tsx
@@ -22,7 +22,7 @@ const DashboardNav = () => {
 					<span className={cn(
 						"group flex items-center rounded-md px-3 py-2 text-sm font-medium hover:bg-accent hover:text-accent-foreground", pathname === item.href ? "bg-accent" : "bg-transparent"
 					)}>
-						<item.icon className='mr-2 h-4 w-4' />
+						<item.icon className='mr-2 h-4 w-4 text-primary' />
 						<span>{item.name}</span>
 					</span>
 				</Link>
