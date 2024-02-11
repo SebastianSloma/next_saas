b52738ae1061cad35ec86ed2cf7e6df6d59860db
Sebastian Słoma <sebastiansloma@gmail.com>
2024-02-11 11:59:44 +0100

    pathname

--- app/components/DashboardNav.tsx
+++ app/components/DashboardNav.tsx
@@ -20,7 +20,7 @@ const DashboardNav = () => {
 			{navItems.map((item, index) => (
 				<Link key={index} href={item.href}>
 					<span className={cn(
-						"group flex items-center rounded-md px-3 py-2 text-sm font-medium hover:bg-accent hover:text-accent-foreground"
+						"group flex items-center rounded-md px-3 py-2 text-sm font-medium hover:bg-accent hover:text-accent-foreground", pathname === item.href ? "bg-accent" : "bg-transparent"
 					)}>
 						<item.icon className='mr-2 h-4 w-4' />
 						<span>{item.name}</span>
