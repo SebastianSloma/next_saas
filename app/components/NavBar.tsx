a345b1e3cc325193ac6142d3af142fd4d8b73e0a
Sebastian Słoma <sebastiansloma@gmail.com>
2024-02-11 13:21:51 +0100

    fix bug with get user function

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -9,26 +9,33 @@ import {
 } from '@kinde-oss/kinde-auth-nextjs/components';
 
 import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server';
-
+import { UserNav } from './UserNav';
 
 export async function NavBar() {
-	const { isAuthenticated } = getKindeServerSession();
+	const { isAuthenticated, getUser} = getKindeServerSession();
+	const user = await getUser();
+
 	return (
 		<nav className='border-b bg-background h-[10vh] flex items-center'>
 			<div className='container flex items-center justify-between'>
 				<Link href='/'>
-					<h1 className='font-bold text-3xl'>Saas <span className='text-primary'>App</span></h1>
+					<h1 className='font-bold text-3xl'>
+						Saas <span className='text-primary'>App</span>
+					</h1>
 				</Link>
 
 				<div className='flex items-center gap-x-5'>
 					<ThemeToggle />
 					{(await isAuthenticated()) ? (
-						<LogoutLink>
-							<Button>
-						
-								Log out</Button>
-						</LogoutLink>
+						<UserNav
+							email={user?.email as string}
+							image={user?.picture as string}
+							name={user?.given_name as string}
+						/>
 					) : (
+						// <LogoutLink>
+						// 	<Button>Log out</Button>
+						// </LogoutLink>
 						<div className='flex items-center gap-x-5'>
 							<LoginLink>
 								<Button>Sign In</Button>
@@ -42,4 +49,7 @@ export async function NavBar() {
 			</div>
 		</nav>
 	);
+}
+function getUser() {
+	throw new Error('Function not implemented.');
 }
--- app/components/UserNav.tsx
+++ app/components/UserNav.tsx
@@ -10,8 +10,10 @@ import {
 	DropdownMenuTrigger,
 } from '@/components/ui/dropdown-menu';
 
+import { LogoutLink } from '@kinde-oss/kinde-auth-nextjs/components';
+
 import Link from 'next/link';
-import { CreditCard, Home, Settings } from 'lucide-react';
+import { CreditCard, DoorClosed, Home, Settings } from 'lucide-react';
 
 export const navItems = [
 	{ name: 'Home', href: '/dashboard', icon: Home },
@@ -19,37 +21,61 @@ export const navItems = [
 	{ name: 'Billing', href: '/dashboard/billing', icon: CreditCard },
 ];
 
-export function UserNav() {
-	<DropdownMenu>
-		<DropdownMenuTrigger asChild>
-			<Button variant='ghost' className='relative h-10 rounded-full'>
-				<Avatar className='h-10 w-10 rounded-full'>
-					<AvatarImage src='https://github.com/shadcn.png' alt='@shadcn' />
-					<AvatarFallback>Test Name</AvatarFallback>
-				</Avatar>
-			</Button>
-		</DropdownMenuTrigger>
-		<DropdownMenuContent className='w-56' align='end' forceMount>
-			<DropdownMenuLabel>
-				<div className='flex flex-col space-y-1'>
-					<p className='text-sm font-medium leading-none'>Test Name</p>
-					<p className='text-xs leading-none text-muted-foreground'>
-						test@name.com
-					</p>
-				</div>
-			</DropdownMenuLabel>
-			<DropdownMenuSeparator />
-			<DropdownMenuGroup>
-				{navItems.map((item, index) => (
-					<DropdownMenuItem asChild key={index}>
-						<Link
-							href={item.href}
-							className='w-full flex justify-center items-center'>
-							{item.name}
-						</Link>
-					</DropdownMenuItem>
-				))}
-			</DropdownMenuGroup>
-		</DropdownMenuContent>
-	</DropdownMenu>;
+export function UserNav({
+	name,
+	email,
+	image,
+}: {
+	name: string;
+	email: string;
+	image: string;
+}) {
+	return (
+		<DropdownMenu>
+			<DropdownMenuTrigger asChild>
+				<Button variant='ghost' className='relative h-10 rounded-full'>
+					<Avatar className='h-10 w-10 rounded-full'>
+						<AvatarImage src={image} alt='' />
+						<AvatarFallback>Test Name</AvatarFallback>
+					</Avatar>
+				</Button>
+			</DropdownMenuTrigger>
+			<DropdownMenuContent className='w-56' align='end' forceMount>
+				<DropdownMenuLabel>
+					<div className='flex flex-col space-y-1'>
+						<p className='text-sm font-medium leading-none'>{name}</p>
+						<p className='text-xs leading-none text-muted-foreground'>
+							{email}
+						</p>
+					</div>
+				</DropdownMenuLabel>
+				<DropdownMenuSeparator />
+				<DropdownMenuGroup>
+					{navItems.map((item, index) => (
+						<DropdownMenuItem asChild key={index}>
+							<Link
+								href={item.href}
+								className='w-full flex justify-center items-center'>
+								{item.name}
+								<span>
+									<item.icon className='w-4 h-4' />
+								</span>
+							</Link>
+						</DropdownMenuItem>
+					))}
+				</DropdownMenuGroup>
+				<DropdownMenuSeparator />
+				<DropdownMenuItem
+					className='w-full flex justify-between items-center'
+					asChild>
+					<LogoutLink>
+						Logout{' '}
+						<span>
+							<DoorClosed className=' w-4 h-4' />
+						</span>
+					</LogoutLink>
+				</DropdownMenuItem>
+			</DropdownMenuContent>
+		</DropdownMenu>
+	);
 }
