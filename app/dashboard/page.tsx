eab378cc3c1b29de058f3f2c13a549dabb16c26d
Sebastian Słoma <sebastiansloma@gmail.com>
2024-02-14 13:03:24 +0100

    added btc widget

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,5 +1,14 @@
 import React from 'react';
 
 export default function DashboardPage() {
-	return <div>Dashboard SaaS</div>;
+	return (
+		<div>
+			<iframe
+				src='https://coin360.com/coin-widget?coin=bitcoin-btc&utm_source=embed_widget_coin'
+				width='50%'
+				height='252'
+				
+			/>
+		</div>
+	);
 }
