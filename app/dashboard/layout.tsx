d9b6f27c202b5f094243bfab5686c574b827faf7
Sebastian Słoma <sebastiansloma@gmail.com>
2024-02-18 16:57:32 +0100

    add f getData

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -2,12 +2,33 @@ import React, { ReactNode } from 'react';
 import DashboardNav from '../components/DashboardNav';
 import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server';
 import { redirect } from 'next/navigation';
+import prisma from '../lib/db';
 
-export default async function DashboardLayout({ children }: { children: ReactNode }) {
-	const {getUser} = getKindeServerSession()
-	const user = await getUser
-	if(!user){
-		return redirect
+async function getData({
+	email,
+	id,
+	firstName,
+	lastName,
+	profileImage,
+}: {
+	email: string;
+	id: string;
+	firstName: string | undefined | null;
+	lastName: string | undefined | null;
+	profileImage: string | undefined | null;
+}) {
+	const user = await prisma;
+}
+
+export default async function DashboardLayout({
+	children,
+}: {
+	children: ReactNode;
+}) {
+	const { getUser } = getKindeServerSession();
+	const user = await getUser;
+	if (!user) {
+		return redirect;
 	}
 	return (
 		<div className='flex flex-col space-y-6 mt-10'>
