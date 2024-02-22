4e51c2e35dc76c336ef501a04e2c725a096b8ef4
Sebastian Słoma <sebastiansloma@gmail.com>
2024-02-22 22:54:59 +0100

    d

--- app/dashboard/settings/page.tsx
+++ app/dashboard/settings/page.tsx
@@ -2,7 +2,9 @@ export default function SettingPage() {
 	return (
         <div className='grid items-start gap-8'>
             <div className='flex items-center justify-between px-2'>
-                
+                <div className='grid gap-1'>
+                    
+                </div>
 
             </div>
         </div>
