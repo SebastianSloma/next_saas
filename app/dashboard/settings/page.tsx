8157d48b9ab6ed0b93b99f56d26ba342d2152200
Sebastian Słoma <sebastiansloma@gmail.com>
2024-02-23 07:51:44 +0100

    added card

--- app/dashboard/settings/page.tsx
+++ app/dashboard/settings/page.tsx
@@ -1,14 +1,13 @@
 export default function SettingPage() {
 	return (
-        <div className='grid items-start gap-8'>
-            <div className='flex items-center justify-between px-2'>
-                <div className='grid gap-1'>
-                    <h1 className='text-3xl md:text-4xl'>Settings</h1>
-                    <p className='text-lg text-muted-foreground'>Your Profile Settings</p>
-                    
-                </div>
-
-            </div>
-        </div>
-    );
+		<div className='grid items-start gap-8'>
+			<div className='flex items-center justify-between px-2'>
+				<div className='grid gap-1'>
+					<h1 className='text-3xl md:text-4xl'>Settings</h1>
+					<p className='text-lg text-muted-foreground'>Your Profile Settings</p>
+				</div>
+			</div>
+            
+		</div>
+	);
 }
