e68cb44a04b361a57fea6914dde7348d311e4683
Sebastian Słoma <sebastiansloma@gmail.com>
2024-03-06 21:18:49 +0100

    add eth

--- app/dashboard/billing/page.tsx
+++ app/dashboard/billing/page.tsx
@@ -1,5 +1,12 @@
-export default function BillingPage(){
-    return(
-        <div>billing page</div>
-    )
+export default function BillingPage() {
+	return (
+		<div>
+			<iframe
+				src='https://coin360.com/coin-widget?coin=ethereum-eth&utm_source=embed_widget_coin'
+				width='100%'
+				height='252'
+				
+			/>
+		</div>
+	);
 }
