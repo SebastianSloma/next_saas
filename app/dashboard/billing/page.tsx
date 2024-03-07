abce3667663a9e4297204b6226e70f6b467b2b74
Sebastian Słoma <sebastiansloma@gmail.com>
2024-03-07 08:55:34 +0100

    .

--- app/dashboard/billing/page.tsx
+++ app/dashboard/billing/page.tsx
@@ -1,6 +1,6 @@
 export default function BillingPage() {
 	return (
-		<div>
+		<div className='max-w-md mx-auto space-y-4'>
 			<iframe
 				src='https://coin360.com/coin-widget?coin=ethereum-eth&utm_source=embed_widget_coin'
 				width='100%'
