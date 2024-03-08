b4a9d721844f0a6849d947483078351217b5fa6a
Sebastian Słoma <sebastiansloma@gmail.com>
2024-03-08 08:09:21 +0100

    add card

--- app/dashboard/billing/page.tsx
+++ app/dashboard/billing/page.tsx
@@ -1,6 +1,15 @@
+import { Card, CardContent } from '@/components/ui/card';
+
 export default function BillingPage() {
 	return (
 		<div className='max-w-md mx-auto space-y-4'>
+			<Card className='flex flex-col'>
+				<CardContent className='py-8'>
+					<div>
+						<h3>Monthly</h3>
+					</div>
+				</CardContent>
+			</Card>
 			<iframe
 				src='https://coin360.com/coin-widget?coin=ethereum-eth&utm_source=embed_widget_coin'
 				width='100%'
