7cabfce951def4af20d0c88cf4d7096de629a09b
Sebastian Słoma <sebastiansloma@gmail.com>
2024-03-13 19:39:34 +0100

    getD

--- app/dashboard/billing/page.tsx
+++ app/dashboard/billing/page.tsx
@@ -1,12 +1,17 @@
 import { Button } from '@/components/ui/button';
 import { Card, CardContent } from '@/components/ui/card';
 import { CheckCircle2 } from 'lucide-react';
+import prisma from '@/app/lib/db'
 
-const featureItems =[
-	{name: 'note number one'},
-	{name: 'note number two'},
-	{name: 'note number three'},
-]
+const featureItems = [
+	{ name: 'note number one' },
+	{ name: 'note number two' },
+	{ name: 'note number three' },
+];
+
+async function getData() {
+	const data = await prisma;
+}
 
 export default function BillingPage() {
 	return (
@@ -14,7 +19,9 @@ export default function BillingPage() {
 			<Card className='flex flex-col'>
 				<CardContent className='py-8'>
 					<div>
-						<h3 className='inline-flex px-4 py-1 rounded-full text-sm font-semibold tracking-wide uppercase bg-primary/10 text-primary'>Monthly</h3>
+						<h3 className='inline-flex px-4 py-1 rounded-full text-sm font-semibold tracking-wide uppercase bg-primary/10 text-primary'>
+							Monthly
+						</h3>
 					</div>
 					<div className='mt-4 flex items-baseline text-6xl font-extrabold'>
 						30€<span className='ml-1 text-2xl text-muted-foreground'>/mo</span>
@@ -25,16 +32,16 @@ export default function BillingPage() {
 				</CardContent>
 				<div className='flex-1 flex flex-col justify-between px-6 pt-6 pb-8 bg-secondary rounded-lg m-1 space-y-6 sm:p-10 sm:pt-6'>
 					<ul className='space-y-6'>
-						{featureItems.map((item,index) =>(
+						{featureItems.map((item, index) => (
 							<li key={index} className='flex items-center'>
 								<div className='flex-shrink-0'>
-									<CheckCircle2 className='h-6 w-6 text-green-500'/>
+									<CheckCircle2 className='h-6 w-6 text-green-500' />
 								</div>
 								<p className='ml-3 text-base'>{item.name}</p>
 							</li>
 						))}
 					</ul>
-					<form action="" className='w-full'>
+					<form action='' className='w-full'>
 						<Button className='w-full'>Buy Today</Button>
 					</form>
 				</div>
@@ -43,7 +50,6 @@ export default function BillingPage() {
 				src='https://coin360.com/coin-widget?coin=ethereum-eth&utm_source=embed_widget_coin'
 				width='100%'
 				height='252'
-				
 			/>
 		</div>
 	);
