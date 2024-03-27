aa1d15300927a94adf7d4f31410f794c8a9e3f89
Sebastian Słoma <sebastiansloma@gmail.com>
2024-03-27 16:39:37 +0100

    crd

--- app/payment/success/page.tsx
+++ app/payment/success/page.tsx
@@ -1,7 +1,11 @@
-export default function CancelledRoute(){
-    return(
-        <div>
-            
-        </div>
-    )
+import { Card } from '@/components/ui/card';
+
+export default function CancelledRoute() {
+	return (
+		<div className='w-full min-h-[80vh] flex items-center justify-center'>
+			<Card className='w-[350px]'>
+
+            </Card>
+		</div>
+	);
 }
